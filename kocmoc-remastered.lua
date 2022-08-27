9384ac1bef85cc5dfc3cc63c83b6fc2d9ae621d5
Schervi <82899388+Schervi@users.noreply.github.com>
2022-08-27 12:28:01 +0100

    Update kocmoc-remastered.lua

--- kocmoc-remastered.lua
+++ kocmoc-remastered.lua
@@ -7,8 +7,6 @@ Credits:
 [Weuz]: Bee Swarm Simulator, function library (xlp)
 [Devixl]: Bee Swarm Simulator, function library (xlp)
 
-Kocmoc is owned, coded developed and managed by Boxking776 alone. Anyone else who claims to be a developer is an impersonator.
-Kocmoc is mostly open source apart from some premium functions.
 Any attempts at stealing Kocmoc source and using it without credit will result in a valid takedown request *.
 
 * In moderation. You can use sections of code, however you are prohibited from ripping off the entire source as your own.
@@ -18,9 +16,9 @@ Thanks for using kocmoc! ^-^
 ]]
 
 if game.PlaceId == 1537690962 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Boxking776/kocmoc/main/games/bss.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/schervi/kocmoc/games/bss.lua"))()
 elseif game.PlaceId == 6284583030 or game.PlaceId == 7722306047 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Boxking776/kocmoc/main/games/psx.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/schervi/kocmoc/main/games/psx.lua"))()
 else
 	game.Players.LocalPlayer:Kick("Error loadng kocmoc.\n\nNo supported game found.\n\nPlease join a supported game.")
 end
